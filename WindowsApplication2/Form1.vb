76d906c778241e478755271cf1d714a75b6cdca0
bisharababish <62569803+bisharababish@users.noreply.github.com>
2022-11-06 18:45:13 +0200

    master

---
+++ WindowsApplication2/Form1.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form1
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        If PictureBox1.Visible = True Then
+            PictureBox2.Visible = True
+            PictureBox1.Visible = False
+        ElseIf PictureBox2.Visible = True Then
+            PictureBox2.Visible = False
+            PictureBox1.Visible = True
+
+        End If
+
+    End Sub
+End Class
---
+++ WindowsApplication2/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("WindowsApplication2")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("Microsoft")>
+<Assembly: AssemblyProduct("WindowsApplication2")>
+<Assembly: AssemblyCopyright("Copyright © Microsoft 2019")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("6fd8b5b9-2192-4276-9d32-1af391f5d20f")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
